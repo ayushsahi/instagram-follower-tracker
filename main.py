0fd727969c39321625e67de9e1012f6f50c19e2d
ayushsahi <ayushsahi10@gmail.com>
2021-12-25 02:02:43 -0500

    finished functionality

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ import instaloader
 
 L = instaloader.Instaloader()
 
-USER = ''
-PASSWORD = ''
+USER = 'ayushsahi'
+PASSWORD = 'M0nk3y10!_Instagram'
 following_list = []
 follower_list = []
+test = 'test'
 
-
-def login(user: str, password: str) -> None:
-    USER = user
-    PASSWORD = password
+def login() -> None:
     L.login(USER, PASSWORD) 
 
 def get_followers() -> None:
@@ -25,14 +23,25 @@ def get_followers() -> None:
 def get_following() -> None:
     profile = instaloader.Profile.from_username(L.context, USER)
 
-    for follower in profile.get_following():
-        following_list.append(follower.username)
+    for follower in profile.get_followees():
+        if follower.is_verified is False:
+            following_list.append(follower.username)
 
     # print(following_list)
     print(len(following_list))
 
+def generate_users_not_following_back() -> list:
+    output = []
+    for user in following_list:
+        if user not in follower_list:
+            output.append(user)
+
+    print(output)
+    return output
 
 if __name__ == "__main__":
-    login('ayushsahi', 'M0nk3y10!_Instagram')
+    login()
     get_followers()
     get_following()
+    generate_users_not_following_back()
+
